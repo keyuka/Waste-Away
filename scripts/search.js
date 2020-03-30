6b399d9c12685fc45f429c92f06a7daee5391542
Oves Patel <oves.patel98@gmail.com>
2020-03-30 13:25:52 -0700

    created a script to get the information from the database

--- scripts/search.js
+++ scripts/search.js
@@ -1,11 +1,30 @@
 saveFile = function () {
+  
     const name = document.getElementById("userInput");
     var userSearch = name.value;
     console.log(userSearch);
-    var data = db.collection('items').doc(userSearch);
-    data.onSnapshot(function (c) {
-        console.log("Current data: ", c.data());
-        document.getElementById("here").innerHTML = c.data().value;
-        });
- 
+
+    
+    db.collection('items').doc(userSearch).get().then(function (snap) {
+        document.getElementById("name").innerHTML = snap.data().name;})
+
+    db.collection('items').doc(userSearch).get().then(function (snap) {
+    var img = document.createElement("IMG");
+    img.src = "images/" + snap.data().picture_ref;
+    document.getElementById("image").appendChild(img);})
+
+
+    
+    db.collection('items').doc(userSearch).get().then(function (snap) {
+        document.getElementById("location").innerHTML = snap.data().disposal_location;})
+
+     db.collection('items').doc(userSearch).get().then(function (snap) {
+            document.getElementById("method").innerHTML = snap.data().disposal_method;})
+
+         
+            
+
+
+
+    //window.location.href = "HTML/results.html";
 }
